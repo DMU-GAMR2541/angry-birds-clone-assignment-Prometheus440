a6385bd0d3a7db717ab62d89e9fe6d9b646874de
Matt Gilks <matthewgilks05@icloud.com>
2026-05-12 14:17:23 +0100

    clean up 2

--- include/Bird.h
+++ include/Bird.h
@@ -12,26 +12,10 @@ class Bird : public DynamicObject {
 
 
 	public:
-		//SFML variables
-		sf::Sprite sp_rendered;
-		sf::Texture sf_tex;
-
 		//Constructor
 		Bird(std::string texPath, float x, float y, float scaleFactor)
 		{
-			spawnX = x;
-			spawnY = y;
-			scale = scaleFactor;
 
-			if (!sf_tex.loadFromFile(texPath)) // Load texture from file path, if fail then message
-			{
-				std::cout << "Failed to load pig texture" << std::endl;
-			}
-
-			sp_rendered.setTexture(sf_tex); // Assign texture to the sprite
-			sp_rendered.setPosition(spawnX, spawnY); // Set the position
-			sp_rendered.setOrigin(sf_tex.getSize().x / 2.0f, sf_tex.getSize().y / 2.0f); // Align the texture to the centre of the instance
-			sp_rendered.setScale(scaleFactor, scaleFactor); // Scale down the bird texture
 		}
 
 		// Destructor
--- include/DynamicObject.h
+++ include/DynamicObject.h
@@ -2,10 +2,30 @@
 #include "GameObject.h"
 
 class DynamicObject : public virtual GameObject{ // Virtual to stop ambiguity for UI inheritance
-	public:
-	// Default constructor and destructor
-	DynamicObject() = default;
+protected:
+	//SFML variables
+	sf::Sprite sp_rendered;
+	sf::Texture sf_tex;
+
+public:
+	// Constructor
+	DynamicObject(std::string texPath, float x, float y, float scale)
+	{
+		if (!sf_tex.loadFromFile(texPath)) // Load texture from file path, if fail then message
+		{
+			std::cout << "Failed to load dynamic object texture" << std::endl;
+		}
+
+		sp_rendered.setTexture(sf_tex); // Assign texture to the sprite
+		sp_rendered.setPosition(x, y); // Set the position
+		sp_rendered.setOrigin(sf_tex.getSize().x / 2.0f, sf_tex.getSize().y / 2.0f); // Align the texture to the centre of the instance
+		sp_rendered.setScale(scale, scale); // Scale down the texture
+	}
+
+	// Default destructor
 	~DynamicObject() override = default;
+
+
 
 	// Override the pure virtual functions from GameObject
 	void update() override
